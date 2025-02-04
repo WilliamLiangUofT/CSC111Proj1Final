9f703bb3674edc9aaf77d20b9a0ee05a959e13bb
WilliamLiangUofT <bill.c.liang@gmail.com>
2025-02-04 17:30:36 -0500

    Update game_entities.py

--- game_entities.py
+++ game_entities.py
@@ -29,11 +29,9 @@ class Player:
 
     Instance Attributes:
         - _inventory: A list of item (or key) names currently held by the player.
-        - current_weight: The total weight carried by the player.
         - score: The player's current score.
     """
     _inventory: list[str]
-    current_weight: float
     score: int
 
     def add_item(self, item_to_be_added: str) -> None:
@@ -59,8 +57,11 @@ class Player:
         Print the player's inventory to the console.
         """
         print("Your inventory has items:")
-        for item in self._inventory:
-            print("- " + item)
+        if len(self._inventory) == 0:
+            print("NO ITEMS")
+        else:
+            for item in self._inventory:
+                print("- " + item)
         print()
 
 
@@ -121,15 +122,11 @@ class Item:
 
     Instance Attributes:
         -name: The name of the item.
-        -description: A description of the item.
-        -start_position: The location ID where the item initially appears.
         -weight: The weight of the item (non-negative).
         -the_key: Optional; the name of the key required to obtain this item.
         -puzzle_to_obtain: Optional; the puzzle ID that must be solved to obtain this item.
     """
     name: str
-    description: str
-    start_position: int
     weight: float
     the_key: Optional[str]
     puzzle_to_obtain: Optional[int]
@@ -141,19 +138,15 @@ class Key:
     A key in our text adventure game world.
 
     Instance Attributes:
-        key_name: The name of the key.
-        description: The description of the key.
-        start_position: The location ID where the key initially appears.
-        weight: The weight of the key (should be 0).
-        puzzle_to_obtain: The puzzle ID that must be solved to obtain the key.
-        the_item: The name of the item that this key unlocks.
+        -key_name: The name of the key.
+        -weight: The weight of the key (should be 0).
+        -puzzle_to_obtain: The puzzle ID that must be solved to obtain the key.
+        -the_item: The name of the item that this key unlocks.
 
     Representation Invariants:
         - weight is 0.
     """
     key_name: str
-    description: str
-    start_position: int
     weight: float
     puzzle_to_obtain: int
     the_item: str
